eddc5ebfa43151450df9892943600e5b056a30ed
jasiellp <jasiellp@hotmail.com>
2015-01-23 16:42:35 -0200

    segundo commit dia 23-01-2015

--- src/br/original/main/main_captura.java
+++ src/br/original/main/main_captura.java
@@ -6,10 +6,19 @@ import com.olf.openjvs.IScript;
 import com.olf.openjvs.OConsole;
 import com.olf.openjvs.OException;
 
+/*
+ * desenvolvido por jasiel
+ * 
+ * 
+ * */
+
 public class main_captura implements IScript
 {
+	
+	private int j = 0;
+	
     public void execute(IContainerContext context) throws OException
     {
-        OConsole.oprint("Hello World!");
+        OConsole.oprint("Hello World! "+j);
     }
 }
